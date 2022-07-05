62fbc317e4338ec6978a830c4101ce6d707dad59
Rufet <rufet.kerimli.2015@mail.ru>
2022-07-06 00:28:12 +0400

    F

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTtitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTtitle("rufat");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpensesFilter>
+      {filteredExpenses.length === 0 ? (
+        <p className="white">Not found</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        ))
+      )}
+    </Card>
+  );
+};
+
+export default Expenses;
